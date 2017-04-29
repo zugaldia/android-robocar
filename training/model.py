8661310d93aa5ca60bafbb52f1a02d1ae66e6070
Antonio Zugaldia <antonio@zugaldia.net>
2017-04-28 22:44:26 -0400

    introduce generators and loss visualization

--- training/model.py
+++ training/model.py
@@ -1,64 +1,99 @@
+"""
+
+TODO:
+- Export model in TF format
+- Reduce images size
+
+"""
+
 import csv
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
-DATA_FOLDER = 'data/'
-CSV_FILE = DATA_FOLDER + 'driving_log.csv'
+#
+# You shouldn't need to modify anything below this section.
+#
+
+INPUT_DATA_FOLDER = 'data/'
+INPUT_CSV_FILE = INPUT_DATA_FOLDER + 'driving_log.csv'
+
+OUTPUT_MODEL = 'model.h5'
+OUTPUT_MODEL_CHART = 'model.png'
+
+CORRECTION = 0.2
+EPOCHS = 5
 
 #
 # Read CSV file
 #
 
 lines = []
-print('Reading %s...' % CSV_FILE)
-with open(CSV_FILE) as csv_file:
+print('Reading %s...' % INPUT_CSV_FILE)
+with open(INPUT_CSV_FILE) as csv_file:
     reader = csv.reader(csv_file)
     for line in reader:
         if line[0].startswith('IMG/'):
-            lines.append(line)
+            steering = float(line[3])
+
+            # Augment the data right away so that it gets randomized
+            lines.append({'path': line[0], 'steering': steering, 'flip': False})  # center
+            lines.append({'path': line[1], 'steering': steering + CORRECTION, 'flip': False})  # left
+            lines.append({'path': line[2], 'steering': steering - CORRECTION, 'flip': False})  # right
+
+            # Augment data by flipping the images horizontally
+            lines.append({'path': line[0], 'steering': steering, 'flip': True})  # center
+            lines.append({'path': line[1], 'steering': steering + CORRECTION, 'flip': True})  # left
+            lines.append({'path': line[2], 'steering': steering - CORRECTION, 'flip': True})  # right
+
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+print('Samples found: %d (training: %d, validation: %d).'
+      % (len(lines), len(train_samples), len(validation_samples)))
 
 #
-# Read training data
+# Generators
 #
 
-count = 0
-correction = 0.2
-images, measurements = [], []
-for line in lines:
-    count += 1
-    # 0 center, 1 left, 2 right, 3 steering, 4 throttle, 5 brake, 6 speed
-    print('[%d/%d] Reading %s...' % (count, len(lines), DATA_FOLDER + line[0]))
-    images.extend([
-        cv2.imread(DATA_FOLDER + line[0].strip()),  # center
-        cv2.imread(DATA_FOLDER + line[1].strip()),  # left
-        cv2.imread(DATA_FOLDER + line[2].strip())])  # right
-    measurements.extend([
-        float(line[3]),  # center
-        float(line[3]) + correction,  # left
-        float(line[3]) - correction])  # right
 
-#
-# Augment data by flipping the images horizontally
-#
+def read_image(path, flip):
+    image = cv2.imread(INPUT_DATA_FOLDER + path.strip())
+    if image is None:
+        raise Exception('Failed to read: %s' % path)
+    if flip:
+        # 1 means flipping around y-axis
+        image = cv2.flip(image, 1)
+    if image is None:
+        raise Exception('Failed to flip: %s' % path)
+    return image
 
-print('Augmenting data...')
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement)
-    augmented_measurements.append(measurement * -1.0)
 
-#
-# Get data in numpy format
-#
+def read_measurement(measurement, flip):
+    return -measurement if flip else measurement
 
-assert len(augmented_images) == len(augmented_measurements)
-print('Converting training %d elements...' % len(augmented_images))
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:  # Loop forever so the generator never terminates
+        samples = shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            images = []
+            measurements = []
+            batch_samples = samples[offset:offset + batch_size]
+            for batch_sample in batch_samples:
+                images.append(read_image(batch_sample['path'], batch_sample['flip']))
+                measurements.append(read_measurement(batch_sample['steering'], batch_sample['flip']))
+            # Convert data to numpy format
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield shuffle(X_train, y_train)
+
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 #
 # Network
@@ -83,16 +118,39 @@ model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
-
 model.add(Flatten())
-
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# Perform training
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+history_object = model.fit_generator(train_generator,
+                                     samples_per_epoch=len(train_samples),
+                                     validation_data=validation_generator,
+                                     nb_val_samples=len(validation_samples),
+                                     nb_epoch=EPOCHS, verbose=1)
 
-model.save('model.h5')
+# Save result
+model.save(OUTPUT_MODEL)
+
+#
+# Visualize loss
+#
+
+# Plot the training and validation loss for each epoch
+print('Generating loss chart...')
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig(OUTPUT_MODEL_CHART)
+
+#
+# Done
+#
+
 print('Done.')
