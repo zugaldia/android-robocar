a9003bbe8ecda379938b65954582573129699789
hsalameh <halim.salameh@gmail.com>
2017-05-20 01:31:42 -0400

    Changed speed slider control range and step values

--- mobile1/app/src/main/java/com/zugaldia/robocar/mobile1/SpeedPercentageMap.java
+++ mobile1/app/src/main/java/com/zugaldia/robocar/mobile1/SpeedPercentageMap.java
@@ -4,24 +4,23 @@ import java.util.TreeMap;
 
 public class SpeedPercentageMap {
 
-    private static final TreeMap<Integer, Integer> speedMap;
-
-    static {
-        speedMap = new TreeMap<Integer, Integer>();
-
-        speedMap.put(0, 0); // %0-%10 => 0 speed
-        speedMap.put(10, 118); // %10-%20 => 118 speed
-        speedMap.put(20, 140); //%20-%30 => 140 speed
-        speedMap.put(30, 162);
-        speedMap.put(40, 184);
-        speedMap.put(50, 206);
-        speedMap.put(60, 228);
-        speedMap.put(70, 255); // %70 and up => 255 speed
-    }
-
     public static int getSpeedForPercentage(int percentage) {
 
-        int speed = speedMap.floorEntry(percentage).getValue();
+        float minPercent = 10;
+        float maxPercent = 80;
+        float minSpeed = 100;
+        float maxSpeed = 255;
+        float percentageRange = maxPercent - minPercent;
+        float speedRange = maxSpeed - minSpeed;
+        float changeRate = speedRange / percentageRange;
+        int speed = (int)( changeRate * ( percentage - minPercent ) + minSpeed );
+        speed = speed - (speed % 10);
+
+        if(speed > maxSpeed)
+            speed = (int) maxSpeed;
+
+        if(speed < minSpeed)
+            speed = 0;
 
         return speed;
     }
