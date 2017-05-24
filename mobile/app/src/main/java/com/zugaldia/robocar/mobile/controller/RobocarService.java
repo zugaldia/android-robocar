176c7d6d785d4f7f3e5abb01acb69ea3d29c92a9
danesfeder <dan.nesfeder@mapbox.com>
2017-05-23 20:15:53 -0400

    Merge mobile / mobile1 - create separate packages for each and launch with mobile1

---
+++ mobile/app/src/main/java/com/zugaldia/robocar/mobile/controller/RobocarService.java
@@ -0,0 +1,12 @@
+package com.zugaldia.robocar.mobile.controller;
+
+import com.zugaldia.robocar.software.webserver.models.RobocarSpeed;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RobocarService {
+    @POST("api/speed")
+    Call<RobocarSpeed> changeSpeed(@Body RobocarSpeed speed);
+}
